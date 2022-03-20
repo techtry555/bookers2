993bd42c4599ddb39251ec722f502c3729dc8e01
EC2 Default User <ec2-user@ip-172-31-82-248.ec2.internal>
2022-03-20 09:00:42 +0000

    一部修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,14 @@ class Book < ApplicationRecord
 
   # Bookモデルで画像を使う宣言
   has_one_attached :image
-  
-  d
 
+  # 画像が表示されない場合にエラーが出るため、その対応策。get_imageメソッドの作成。
+  # この場所(=モデル)に記述することで、このメソッドを(カラムのように)いつでも呼び出せる。
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
 end
