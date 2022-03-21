1ed0565e489720c40b98697ce3418ed35220efb9
EC2 Default User <ec2-user@ip-172-31-82-248.ec2.internal>
2022-03-21 08:38:07 +0000

    バリデーション英語化前

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,13 @@ class Book < ApplicationRecord
   # Bookモデルで画像を使う宣言
   has_one_attached :image
 
+  # バリデーション設定
+  # 空でない
+  validates :title, presence: true
+  # 空でない,かつ最大200文字まで
+  validates :body, presence: true
+  validates :body, length: { maximum: 200 }
+
   # 画像が表示されない場合にエラーが出るため、その対応策。get_imageメソッドの作成。
   # この場所(=モデル)に記述することで、このメソッドを(カラムのように)いつでも呼び出せる。
   def get_image
